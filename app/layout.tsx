81dc8bb232554c56f7bf9a3ad682a1b354fbd63a
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-08-03 01:39:13 +0000

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Acquaah Emmanuel Kofi",
+  description:
+    "My personal portfolio website showcasing projects, skills, experience, and contact info as a software engineer.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
