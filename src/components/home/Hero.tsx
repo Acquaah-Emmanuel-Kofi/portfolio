a46464a8f8eb5cb84b557e81bbd255f44fe175af
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-08-03 23:36:06 +0000

    feat: redesign hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,35 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { EnvelopeClosedIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
 import { contactInfo } from "@/lib/data";
 
 export default function Hero() {
   return (
-    <section
-      id="home"
-      className="container max-w-5xl mx-auto py-12 md:py-16 lg:py-20"
-    >
+    <section id="home" className="py-12 md:py-16 lg:py-20">
       <div className="flex flex-col lg:flex-row items-center justify-center gap-12">
-        <div className="w-1/2 mx-auto lg:w-1/3">
+        <div className="w-1/2 mx-auto lg:w-1/4 border-2 border-white rounded-full overflow-hidden">
           <Image
-            src="/assets/logo.jpg"
+            src="/assets/display-profile.jpeg"
             width={280}
             height={280}
             alt="Developer"
-            className="mx-auto aspect-square overflow-hidden object-cover object-center rounded-full"
+            className="w-full h-full object-cover object-center transition-transform duration-500 hover:scale-110"
           />
         </div>
         <div className="w-full lg:w-2/3 space-y-4">
           <div className="space-y-2">
-            <h1 className="text-4xl md:text-5xl font-bold tracking-tighter ">
-              Hey 👋, I&apos;m Kofi Emmanuel
+            <h1 className="text-4xl md:text-4xl font-bold tracking-tighter ">
+              Hello, I&apos;m Kofi Emmanuel
             </h1>
           </div>
           <p className="max-w-[600px] lg:text-lg text-gray-500 dark:text-gray-400">
-            I am a passionate full-stack developer with a strong background in
-            building modern, scalable web applications. Lets collaborate
-            andbring your ideas to life.
+            I'm a software engineer from Ghana (🇬🇭) who enjoys building clean,
+            performant software that solves real problems and doesn't suck. I
+            care deeply about user experience, maintainable code, and shipping
+            things that actually work.
           </p>
           <div className="space-x-4">
             {contactInfo.map(({ href, icon: Icon, label }) => (
