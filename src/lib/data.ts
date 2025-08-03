46240b3f19af99e1f6c3f957e3c2aafefdd3135a
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-08-03 03:52:45 +0000

    feat: add app data

---
+++ src/lib/data.ts
@@ -0,0 +1,46 @@
+import {
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from "@radix-ui/react-icons";
+
+export const navLinks = [
+  {
+    label: "About",
+    path: "#about",
+  },
+  {
+    label: "Skills",
+    path: "#skills",
+  },
+  {
+    label: "Projects",
+    path: "#projects",
+  },
+  {
+    label: "Contact",
+    path: "#contact",
+  },
+  {
+    label: "Articles",
+    path: "#articles",
+  },
+];
+
+export const contactInfo = [
+  {
+    href: "https://github.com/Acquaah-Emmanuel-Kofi/",
+    icon: GitHubLogoIcon,
+    label: "GitHub",
+  },
+  {
+    href: "https://www.linkedin.com/in/acquaah-emmanuel-kofi/",
+    icon: LinkedInLogoIcon,
+    label: "LinkedIn",
+  },
+  {
+    href: "https://x.com/kofi_waga?s=11",
+    icon: TwitterLogoIcon,
+    label: "X (Twitter)",
+  },
+];
