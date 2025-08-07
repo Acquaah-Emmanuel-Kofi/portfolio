047d38ff5bd9bc0a7bb0a65e046fe0c6e7e573d8
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-08-07 20:44:38 +0000

    feat: animate components

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,5 +1,24 @@
-export function Title({ title }: { title: string }) {
-  return <h2 className="text-3xl font-bold mb-12">{title}</h2>;
+"use client";
+
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+
+export function Title({
+  title,
+  className,
+}: {
+  title: string;
+  className?: string;
+}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+    >
+      <h2 className={cn("text-3xl font-bold mb-12", className)}>{title}</h2>
+    </motion.div>
+  );
 }
 
 export function SubTitle({ title }: { title: string }) {
