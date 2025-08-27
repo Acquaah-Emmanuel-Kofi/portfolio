93f012c3b4fd9687c605c35b4e3ee1835d2c1e65
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-08-27 11:40:43 +0000

    feat: create project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import ProjectList from "@/components/projects/ProjectList";
+
+export default function ProjectsPage() {
+  return (
+    <section className="container max-w-4xl mx-auto px-6">
+      <ProjectList />
+    </section>
+  );
+}
