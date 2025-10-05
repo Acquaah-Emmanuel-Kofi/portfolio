8e9aca8de0d2e63fc3bdb3c40564d98cda36a7af
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-10-05 22:30:18 +0000

    feat: impement mdx utility functions to get and read files

---
+++ src/lib/mdx.ts
@@ -0,0 +1,71 @@
+"use server";
+
+import { promises as fs } from "fs";
+import path from "path";
+import { compileMDX } from "next-mdx-remote/rsc";
+import { ArticleMeta } from "@/types/content";
+
+const articlesDir = path.join(process.cwd(), "src/content/articles");
+
+export const getAllFiles = async () => {
+  try {
+    return await fs.readdir(articlesDir);
+  } catch (error) {
+    console.error("Error reading articles directory:", error);
+    return [];
+  }
+};
+
+export const getFileContent = async (filename: string) => {
+  return await fs.readFile(path.join(articlesDir, filename), "utf-8");
+};
+
+export const getArticleBySlug = async (
+  slug: string,
+  options?: {
+    components?: Record<string, React.ComponentType<any>>;
+  }
+) => {
+  const source = await getFileContent(`${slug}.mdx`);
+
+  return await compileMDX({
+    source,
+    options: {
+      parseFrontmatter: true,
+    },
+    components: {
+      ...(options?.components || {}),
+    },
+  });
+};
+
+export const getAllArticles = async () => {
+  try {
+    const filenames = await getAllFiles();
+
+    if (!filenames.length) return [];
+
+    const posts = await Promise.all(
+      filenames.map(async (filename) => {
+        const source = await getFileContent(filename);
+
+        const { frontmatter } = await compileMDX<ArticleMeta>({
+          source,
+          options: { parseFrontmatter: true },
+        });
+
+        return {
+          ...frontmatter,
+          slug: filename.replace(/\.mdx$/, ""),
+        };
+      })
+    );
+
+    return posts.sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+  } catch (error) {
+    console.error("❌ Error in getAllArticles:", error);
+    return [];
+  }
+};
